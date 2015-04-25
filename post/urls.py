fdea1ff78db549b0477e612213a46af496a617ef
bilbeyt <bilbeyt@itu.edu.tr>
2015-04-25 17:10:09 +0300

    archieve views bug fixed

--- post/urls.py
+++ post/urls.py
@@ -4,9 +4,8 @@ from .models import Entry
 from . import views
 
 urlpatterns = [
-
-    url(r'index/', EntryFormView.as_view(template_name="index.html")),
-    url(r'',EntryView.as_view()),
-    url(r'archieve/april2015', EntryArchieveAprilView.as_view()),
-    url(r'archieve/march2015', EntryArchieveMarchView.as_view()),
+    url(r'^$',EntryView.as_view()),
+    url(r'^index/', EntryFormView.as_view()),
+    url(r'^archieve/april2015/', EntryArchieveAprilView.as_view()),
+    url(r'^archieve/march2015/', EntryArchieveMarchView.as_view()),
 ]
--- post/views.py
+++ post/views.py
@@ -4,14 +4,14 @@ from django.template import RequestContext,loader
 from .models import Entry
 from .forms import EntryCreateForm
 from django.core.urlresolvers import reverse
-from django.views.generic import ListView
+from django.views.generic.list import ListView
 import datetime
 
 
 class EntryFormView(ListView):
     form_class= EntryCreateForm
     initial= { 'key': 'value'}
-    template_name = '/home/tolga856/project/myblog/templates/base.html'
+    template_name="/home/tolga856/project/myblog/templates/index.html"
 
     def get(self,request,*args,**kwargs):
         form = self.form_class(initial=self.initial)
@@ -28,7 +28,7 @@ class EntryFormView(ListView):
 class EntryView(ListView):
     model = Entry
     initial = { 'key' : 'value'}
-    template_name = '/home/tolga856/project/myblog/templates/base.html'
+    template_name="base.html"
 
     def get_context_data(self,**kwargs):
         context = super(EntryView,self).get_context_data(**kwargs)
@@ -38,21 +38,18 @@ class EntryView(ListView):
 class EntryArchieveAprilView(ListView):
     model = Entry
     initial= {'key' : 'value'}
-    template_name = '/home/tolga856/project/myblog/templates/base.html'
-
+    template_name="base.html"
     def get_context_data(self,**kwargs):
         context = super(EntryArchieveAprilView,self).get_context_data(**kwargs)
         context['latest_entry_list']= Entry.objects.filter(pub_date__year=2015).filter(pub_date__month=04)
-
         return context
 
 class EntryArchieveMarchView(ListView):
     model = Entry
     initial= {'key' : 'value'}
-    template_name = '/home/tolga856/project/myblog/templates/base.html'
+    template_name="base.html"
 
     def get_context_data(self,**kwargs):
         context = super(EntryArchieveMarchView,self).get_context_data(**kwargs)
         context['latest_entry_list']= Entry.objects.filter(pub_date__year=2015).filter(pub_date__month=03)
-
         return context
